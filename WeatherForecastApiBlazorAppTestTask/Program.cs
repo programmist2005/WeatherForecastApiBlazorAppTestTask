0b6df4c5db4acdc50ba1580820c6fc8565e5c390
olegz86 <olegz86@gmail.com>
2026-08-14 21:49:14 +0300

    frontent app on blazor

--- WeatherForecastApiBlazorAppTestTask/Program.cs
+++ WeatherForecastApiBlazorAppTestTask/Program.cs
@@ -1,10 +1,27 @@
+using Serilog;
+using WeatherForecastApi.Application;
+using WeatherForecastApi.Infrastructure;
 using WeatherForecastApiBlazorAppTestTask.Components;
 
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
+
+// Configure Serilog
+Log.Logger = new LoggerConfiguration()
+    //.WriteTo.Console()
+    //.MinimumLevel.Debug()
+    .CreateBootstrapLogger();
+
+builder.Host.UseSerilog((hostContext, loggerConfiguration) =>
+            _ = loggerConfiguration.ReadFrom.Configuration(builder.Configuration));
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+// Application services
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(config);
 
 var app = builder.Build();
 
